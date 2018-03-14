0500b662044d9707029a9db311dd98fd6713864d
Naruya Kondo <n-kondo@g.ecc.u-tokyo.ac.jp>
2018-03-14 14:30:13 +0900

    selective search やってみた

--- get_image.sh
+++ get_image.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-python get_image.py data_json data_image
+python get_image.py "data_json/json5" "data_image/image5" & \
+python get_image.py "data_json/json6" "data_image/image6" & \
+python get_image.py "data_json/json7" "data_image/image7" & \
+python get_image.py "data_json/json8" "data_image/image8" & \
+python get_image.py "data_json/json9" "data_image/image9" & \
+python get_image.py "data_json/json10" "data_image/image10"
 wait
 echo done!
